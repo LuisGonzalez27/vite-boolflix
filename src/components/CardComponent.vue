eed475782048056d4f033282ac0ef4778d491217
LuisGonzalez27 <luis27coding@gmail.com>
2022-11-17 17:49:57 +0100

    Milestone 2 e 3 test

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,9 +1,24 @@
 <template>
-    <div class="my-card" style="height: 460px;">
-        <h4>Titolo: {{ resObtained.title }}</h4>
-        <p>Titolo originale: {{ resObtained.original_title }}</p>
-        <p>Lingua: {{ resObtained.original_language }}</p>
-        <p>Voto: {{ resObtained.vote_average }}</p>
+    <div class="my-card" style="height: 520px;">
+        <img :src="imgFilm + resObtained.poster_path" :alt="resObtained.title">
+        <ul class="style-none">
+            <li>
+                <h4>Titolo: {{ resObtained.title }}</h4>
+            </li>
+            <li>
+                <p>Titolo originale: {{ resObtained.original_title }}</p>
+            </li>
+            <li>
+                <p>Voto: {{ resObtained.vote_average }}</p>
+            </li>
+            <li>
+                <p v-if="availableFlag.includes(resObtained.original_language)" class="icon-flag">
+                    <img :src="'/img/' + resObtained.original_language + '.png'"
+                        :alt="resObtained.original_language + 'Flag'">
+                </p>
+                <p v-else> Lingua :{{ resObtained.original_language }}</p>
+            </li>
+        </ul>
     </div>
 </template>
   
@@ -13,6 +28,14 @@ export default {
     props: {
         resObtained: Object,
     },
+    data() {
+        return {
+            availableFlag: [
+                'it', 'de', 'en', 'es', 'fr'
+            ],
+            imgFilm: 'https://image.tmdb.org/t/p/w342'
+        }
+    }
 };
 </script>
   
@@ -30,7 +53,25 @@ export default {
 
     h4 {
         padding-top: 10px;
+        font-size: 15px;
         text-transform: uppercase;
+    }
+
+    ul {
+        list-style-type: none;
+
+        li {
+            p {
+                margin-bottom: 0rem;
+            }
+        }
+    }
+}
+
+.icon-flag {
+    img {
+        height: 20px;
+        width: 30px
     }
 }
 </style>
